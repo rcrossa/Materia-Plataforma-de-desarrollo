3ed7831c6305268c0b7249bea5c3869a93787b09
Roberto Rossa <rcrossa@hotmail.com>
2020-08-25 22:55:51 -0300

    Update Program.cs
    Ejercicio clase 25-ago-2020

--- 25-ago-2020/program/program/Program.cs
+++ 25-ago-2020/program/program/Program.cs
@@ -6,7 +6,17 @@ namespace program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero = 0;
+            Console.Write("ingrese un numero");
+
+            while(numero != 10)
+            {
+                Console.Write("ingrese un numero");
+                numero = int.Parse(Console.ReadLine());
+                Console.WriteLine(numero);
+                
+            }
+            Console.WriteLine("Adivino el numero. Saludos");
         }
     }
 }
