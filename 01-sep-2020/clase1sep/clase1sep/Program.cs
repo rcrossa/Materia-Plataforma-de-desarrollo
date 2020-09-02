71acdb1a35fa82b1a910cdd913be9a6d3c9a60e1
Roberto Rossa <rcrossa@hotmail.com>
2020-09-01 21:07:27 -0300

    clase 1 sep

---
+++ 01-sep-2020/clase1sep/clase1sep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clase1sep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
